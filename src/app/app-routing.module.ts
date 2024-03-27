02fb50d87a6a4ff55c35c421cab4a138683f43d2
achref <achref.smida@esprit.tn>
2024-03-27 01:23:41 +0100

    first_commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CarGestionComponent} from "./car-gestion/car-gestion.component";
+import {AddCarComponent} from "./car-gestion/add-car/add-car.component";
+import {MapComponent} from "./map/map.component";
+import {JourneyComponent} from "./journey/journey.component";
+import {ListJourneyComponent} from "./journey/list-journey/list-journey.component";
+
+const routes: Routes = [
+  { path: 'car_gestion', component:CarGestionComponent},
+  { path: 'add_car', component:AddCarComponent},
+  {path: 'map', component: MapComponent},
+  {path: 'add_journey', component: JourneyComponent},
+  {path: 'list_journey', component: ListJourneyComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { CarGestionComponent } from './car-gestion/car-gestion.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { AddCarComponent } from './car-gestion/add-car/add-car.component';
+import { MapComponent } from './map/map.component';
+import { JourneyComponent } from './journey/journey.component';
+import { ListJourneyComponent } from './journey/list-journey/list-journey.component';
+import {DataService} from "./data.service";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponentComponent,
+    CarGestionComponent,
+    AddCarComponent,
+    MapComponent,
+    JourneyComponent,
+    ListJourneyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [
+    DataService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
